0df778bd4f8ac24c45a1b76db98d413f2ff6e9b4
yangkuo1 <yangkuo1@100tal.com>
2018-09-17 18:14:52 +0800

    axios,flexible

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from './api/http'
 class App extends Component {
-
+  componentWillMount () {
+    console.log('componentWillMount')
+    axios.get('/topics').then((data) => {
+      console.log(data)
+    })
+  }
   render() {
     return (
       <div>
---
+++ src/api/http.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+axios.defaults.baseURL = 'https://cnodejs.org/api/v1'
+axios.defaults.timeout = 5000
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN'
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.interceptors.request.use(function (config) {
+// Do something before request is sent
+return config
+}, function (error) {
+// Do something with request error
+return Promise.reject(error)
+})
+  
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+// Do something with response data
+return response
+}, function (error) {
+// Do something with response error
+return Promise.reject(error)
+})
+export default axios
